407c93412164a0de3ca37b6b5827b41ca5467a80
PyaeKaung <166913448+PyaeKaung@users.noreply.github.com>
2025-03-24 22:16:19 +0630

    Update particles.py

--- particles.py
+++ particles.py
@@ -7,8 +7,8 @@ class Particle:
         self.size = random.randint(10,15)
         self.color = (255, 127, 0)
         self.life = random.randint(30,40)
-        self.vx = random.uniform(-5, -2)
-        self.vy = random.uniform(-5, -2)
+        self.vx = random.uniform(-5, 2)
+        self.vy = random.uniform(-5, 2)
     
     def update(self):
         self.x += self.vx
@@ -17,5 +17,5 @@ class Particle:
 
     def draw(self, surface, camx, camy):
         if self.life > 0:
-            rect = pg.Rect(int(self.x - camx - -5), int(self.y - camy - 0), self.size, self.size)
+            rect = pg.Rect(int(self.x - camx), int(self.y - camy), self.size, self.size)
             pg.draw.rect(surface, self.color, rect)
