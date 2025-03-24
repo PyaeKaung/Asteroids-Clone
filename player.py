9dde866893c2dda66dc1f4a085e5ec9ce6be463a
PyaeKaung <166913448+PyaeKaung@users.noreply.github.com>
2025-03-24 22:16:42 +0630

    Update player.py

--- player.py
+++ player.py
@@ -1,11 +1,13 @@
 import pygame as pg
 
-player_speed_increase = 2
+player_health = 200
+max_health = 200
+player_speed_increase = 1
 player_speed_decrease = 0.1
 player_speed = 0
-forward_player_max_speed = 15
-backward_player_max_speed = -7
-player_rotation_speed = 5
+forward_player_max_speed = 10
+backward_player_max_speed = -5
+player_rotation_speed = 3
 player_angle = 0
 
 world_radius = 6000
